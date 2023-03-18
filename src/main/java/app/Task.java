449cb5fb0f8c19706a518d098b7f0f1fa167046a
rybinvm.24 <valery.rybin.spb@gmail.com>
2023-03-18 10:28:16 +0300

    control beginning

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -85,6 +85,26 @@ public class Task {
     }
 
     /**
+     * Очистить задачу
+     */
+    public void clear() {
+        points.clear();
+    }
+
+    /**
+     * Решить задачу
+     */
+    public void solve() {
+        PanelLog.warning("Вызван метод solve()\n Пока что решения нет");
+    }
+    /**
+     * Отмена решения задачи
+     */
+    public void cancel() {
+
+    }
+
+    /**
      * Рисование задачи
      *
      * @param canvas   область рисования
