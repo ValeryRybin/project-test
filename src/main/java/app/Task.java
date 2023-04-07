8b876d2143afeabe1084b05fbdf9698f98a6762e
rybinvm.24 <valery.rybin.spb@gmail.com>
2023-04-07 12:40:01 +0300

    solve button

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -42,6 +42,11 @@ public class Task {
     private final ArrayList<Point> points;
 
     /**
+     * Флаг, решена ли задача
+     */
+    private boolean solved;
+
+    /**
      * Размер точки
      */
     private static final int POINT_SIZE = 3;
@@ -100,21 +105,30 @@ public class Task {
      */
     public void clear() {
         points.clear();
+        solved = false;
     }
 
     /**
      * Решить задачу
      */
     public void solve() {
+        solved = true;
         PanelLog.warning("Вызван метод solve()\n Пока что решения нет");
     }
     /**
      * Отмена решения задачи
      */
     public void cancel() {
-
+        solved = false;
     }
-
+    /**
+     * проверка, решена ли задача
+     *
+     * @return флаг
+     */
+    public boolean isSolved() {
+        return solved;
+    }
     /**
      * Рисование задачи
      *
@@ -163,6 +177,7 @@ public class Task {
      * @param pointSet множество
      */
     public void addPoint(Vector2d pos, Point.PointSet pointSet) {
+        solved = false;
         Point newPoint = new Point(pos, pointSet);
         points.add(newPoint);
         // Добавляем в лог запись информации
