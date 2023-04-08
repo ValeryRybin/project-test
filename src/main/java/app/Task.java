d0cf9969286531e1e8fb4a202a4a111f5bf3ca72
rybinvm.24 <valery.rybin.spb@gmail.com>
2023-04-08 10:11:05 +0300

    scale works

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -56,6 +56,11 @@ public class Task {
     private static final int POINT_SIZE = 3;
 
     /**
+     * коэффициент колёсика мыши
+     */
+    private static final float WHEEL_SENSITIVE = 0.001f;
+
+    /**
      * Порядок разделителя сетки, т.е. раз в сколько отсечек
      * будет нарисована увеличенная
      */
@@ -151,6 +156,20 @@ public class Task {
 
         // задача решена
         solved = true;
+    }
+
+    /**
+     * Масштабирование области просмотра задачи
+     *
+     * @param delta  прокрутка колеса
+     * @param center центр масштабирования
+     */
+    public void scale(float delta, Vector2i center) {
+        if (lastWindowCS == null) return;
+        // получаем координаты центра масштабирования в СК задачи
+        Vector2d realCenter = ownCS.getCoords(center, lastWindowCS);
+        // выполняем масштабирование
+        ownCS.scale(1 + delta * WHEEL_SENSITIVE, realCenter);
     }
 
     /**
